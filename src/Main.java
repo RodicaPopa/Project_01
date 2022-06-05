8797759ae2d34efb74d512c706fa7633179ac049
Rodica Popa <luchianrodica@yahoo.com>
2022-06-05 16:41:56 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Rodica Popa");
+
+        int numar1 = 33;
+        int numar2 = 4;
+        int rezultat1 = numar1 + numar2;
+        System.out.println(rezultat1);
+
+        float numar3 = 10f;
+        float numar4 = 3f;
+        float rezultat2 = numar3 / numar4;
+        System.out.println(rezultat2);
+
+        int numar5 = -5;
+        int numar6 = 8;
+        int numar7 = 6;
+        int rezultat3= -5 + 8 * 6;
+        System.out.println(rezultat3);
+
+        int numar8 = 55;
+        int numar9 = 9;
+        int rezultat4 = (numar8+numar9) % numar9;
+        System.out.println(rezultat4);
+
+        float numar10 = 20;
+        float numar11 = -3;
+        float numar12 = 5;
+        int numar13 = 8;
+        float rezultat5 = numar10 + numar11 * numar12 / numar13;
+        System.out.println(rezultat5);
+
+
+       int numar14 = 5;
+       float numar15 = 15;
+       float numar16 = 3;
+       int numar17 = 2;
+       int numar18 = 8;
+       int numar19 = 3;
+       float rezultat6 = numar14 + numar15 / numar16 * numar17 - numar18 % numar19;
+        System.out.println(rezultat6);
+    }
+}
+
+
